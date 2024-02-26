3f7265c11adfb42ccf4e9b6e2e66f6812a042e61
Subasifinans33 <subasiberk214@gmail.com>
2024-02-26 10:57:46 +0300

    settings button route implementation;

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,6 +1,6 @@
+import {router} from 'expo-router';
 import _ from 'lodash';
 import React from 'react';
-import {CircleIconButton} from '@components/buttons';
 import {ScreenContainer} from '@components/containers';
 import {HeaderShown} from '@components/header';
 import {
@@ -8,22 +8,22 @@ import {
     ProfileCard1,
     ProfileUpgradeNotice,
 } from '@components/viewProfile';
-import useAppImages from '@hooks/useAppImages';
-import {Icon, Image, Stack, XStack, YStack} from '@ui/primitives';
+import {Icon, Stack, XStack, YStack} from '@ui/primitives';
+import APP_CONFIG from '@constants/APP_CONFIG';
 
-const Profile = () => {
+function Profile() {
     const profileCards = [
         {
             bg: '$primary',
 
-            superLikes: 5 + ' Super Likes',
+            superLikes: `${5} Super Likes`,
             borderColor: '$red10',
             description: 'DAHA FAZLA AL',
             icon: 'AdIcon',
         },
         {
             bg: '$primary',
-            superLikes: 5 + ' Boosts',
+            superLikes: `${5} Boosts`,
             borderColor: '$primaryLight',
             description: 'DAHA FAZLA AL',
             icon: 'AdIcon',
@@ -36,42 +36,47 @@ const Profile = () => {
         },
     ];
 
+    const settingsButton = () => {
+        router.push(APP_CONFIG.APP_PATHS.SETTINGS);
+    };
+
     return (
         <ScreenContainer
             horizontalPadding={0}
             verticalPadding={0}
         >
             <YStack
-                w={'100%'}
-                h={'100%'}
-                ai={'center'}
+                w="100%"
+                h="100%"
+                ai="center"
             >
                 <HeaderShown
                     variant="circular"
                     paddingHorizontal={false}
                 >
                     <XStack
-                        w={'100%'}
-                        h={'100%'}
-                        ai={'flex-start'}
-                        jc={'space-between'}
-                        gap={'$6'}
+                        w="100%"
+                        h="100%"
+                        ai="flex-start"
+                        jc="space-between"
+                        gap="$6"
                     >
                         <XStack
-                            mr={'$15'}
-                            h={'100%'}
-                        ></XStack>
-                        <XStack mt={'$3'}>
+                            mr="$15"
+                            h="100%"
+                        />
+                        <XStack>
                             <Stack>
-                                <CircleIconButton
-                                    icon="Secure"
-                                    variant="tertiary"
+                                <Icon
+                                    name="Secure"
+                                    color="$primary"
                                 />
                             </Stack>
-                            <Stack ml={'$6'}>
-                                <CircleIconButton
-                                    icon="SettingsIcon"
-                                    variant="tertiary"
+                            <Stack ml="$6">
+                                <Icon
+                                    name="SettingsIcon"
+                                    color="$primary"
+                                    onPress={settingsButton}
                                 />
                             </Stack>
                         </XStack>
@@ -79,14 +84,14 @@ const Profile = () => {
                 </HeaderShown>
 
                 <YStack
-                    w={'100%'}
-                    ai={'center'}
+                    w="100%"
+                    ai="center"
                 >
                     <YStack
-                        ai={'center'}
-                        w={'100%'}
-                        borderRadius={'$6'}
-                        mt={'$2'}
+                        ai="center"
+                        w="100%"
+                        borderRadius="$6"
+                        mt="$2"
                     >
                         <ProfileAvatar
                             age={29}
@@ -95,13 +100,13 @@ const Profile = () => {
                     </YStack>
                     <XStack
                         style={{justifyContent: 'center'}}
-                        gap={'$4'}
-                        mt={'$12'}
+                        gap="$4"
+                        mt="$12"
                     >
                         {_.map(profileCards, (card, index) => (
                             <Stack
                                 key={index}
-                                mt={'$6'}
+                                mt="$6"
                             >
                                 <ProfileCard1
                                     bg={card.bg}
@@ -114,13 +119,13 @@ const Profile = () => {
                             </Stack>
                         ))}
                     </XStack>
-                    <Stack mt={'$13'}>
+                    <Stack mt="$13">
                         <ProfileUpgradeNotice onPress={() => {}} />
                     </Stack>
                 </YStack>
             </YStack>
         </ScreenContainer>
     );
-};
+}
 
 export default Profile;
