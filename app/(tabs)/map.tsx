85b6599094ab23fcbb8a7b366a4a6a30d97d259e
Subasifinans33 <subasiberk214@gmail.com>
2024-02-27 11:43:02 +0300

    basic map api implementation

--- app/(tabs)/map.tsx
+++ app/(tabs)/map.tsx
@@ -1,58 +1,76 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {View} from 'react-native';
 import MapView, {Marker} from 'react-native-maps';
-import {Text, YStack} from '@ui/primitives';
+import {ScreenContainer} from '@components/containers';
+import {YStack} from '@ui/primitives';
 
-const MapScreen = () => {
-    // CampingArea
-    const campgrounds = [
-        {
-            id: 1,
-            title: 'İstanbul Camping Area',
-            description: 'Bu bir kamp alanıdır.',
-            latitude: 41.0082,
-            longitude: 28.9784,
-        },
-        {
-            id: 2,
-            title: 'Berlin tiny house',
-            description: 'Bu bir kamp alanıdır.',
-            latitude: 52.52,
-            longitude: 13.405,
-        },
-    ];
+function MapScreen() {
+    const [campgrounds, setCampgrounds] = useState([]);
+
+    const fetchCampgrounds = async () => {
+        try {
+            const response = await fetch(
+                'https://overpass-api.de/api/interpreter?data=[out:json];(node["tourism"="caravan_site"];way["tourism"="caravan_site"];relation["tourism"="caravan_site"];);out;',
+            );
+            const data = await response.json();
+            console.log('API Response:', data);
+            if (data.elements && data.elements.length > 0) {
+                const campgroundsData = data.elements.map(
+                    (element: any) => ({
+                        id: element.id,
+                        title: element.tags.name || 'Unknown',
+                        latitude: element.lat,
+                        longitude: element.lon,
+                    }),
+                );
+                setCampgrounds(campgroundsData);
+            } else {
+                console.error('No campgrounds data found.');
+            }
+        } catch (error) {
+            console.error('Error fetching campgrounds:', error);
+        }
+    };
+
+    useEffect(() => {
+        fetchCampgrounds();
+    }, []);
 
     return (
-        <YStack
-            w={'100%'}
-            h={'100%'}
+        <ScreenContainer
+            headerPadding
+            horizontalPadding={0}
         >
-            <View style={{flex: 1}}>
-                <MapView
-                    style={{flex: 1}}
-                    initialRegion={{
-                        latitude: 41.0082, // Default Value ( ISTANBUL )
-                        longitude: 28.9784,
-                        latitudeDelta: 10, // Zoom Default Value
-                        longitudeDelta: 10,
-                    }}
-                >
-                    {/* Camping Area Marker */}
-                    {campgrounds.map((campground) => (
-                        <Marker
-                            key={campground.id}
-                            coordinate={{
-                                latitude: campground.latitude,
-                                longitude: campground.longitude,
-                            }}
-                            title={campground.title}
-                            description={campground.description}
-                        />
-                    ))}
-                </MapView>
-            </View>
-        </YStack>
+            <YStack
+                w="100%"
+                h="100%"
+            >
+                <View style={{flex: 1}}>
+                    <MapView
+                        style={{flex: 1}}
+                        initialRegion={{
+                            latitude: 41.0082,
+                            longitude: 28.9784,
+                            latitudeDelta: 10,
+                            longitudeDelta: 10,
+                        }}
+                    >
+                        {/* Kamp alanı işaretçileri */}
+                        {campgrounds.map((campground) => (
+                            <Marker
+                                key={campground.id}
+                                coordinate={{
+                                    latitude: campground.latitude,
+                                    longitude: campground.longitude,
+                                }}
+                                title={campground.title}
+                            />
+                        ))}
+                    </MapView>
+                </View>
+            </YStack>
+        </ScreenContainer>
     );
-};
+}
 
 export default MapScreen;
